75d05760a1261c73e02988dde7fde98bc478130b
André <111359909+A-thepuma@users.noreply.github.com>
2025-09-02 15:22:02 +0200

    Organisez le code PHP en blocs

--- index.php
+++ index.php
@@ -12,16 +12,7 @@
     <header>
         <!-- Le menu -->
     
-        <nav id="menu">        
-            <div class="element_menu">
-                <h3>Menu de navigation</h3>
-                <ul>
-                    <li><a href="page1.html">Lien</a></li>
-                    <li><a href="page2.html">Lien</a></li>
-                    <li><a href="page3.html">Lien</a></li>
-                </ul>
-            </div>    
-        </nav>
+        <?php include('header.php'); ?>
        
     </header>
     
